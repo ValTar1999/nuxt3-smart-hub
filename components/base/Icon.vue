b3a2d98dabee41d87c54ffbe40c693959c1a310f
ValTar1999 <vladtarna25@gmail.com>
2026-01-08 14:10:57 +0200

    update deploy

--- components/base/Icon.vue
+++ components/base/Icon.vue
@@ -8,6 +8,10 @@
 <script lang="ts" setup>
 import type { TIcon } from '~/enums/common/iconList'
 
+defineOptions({
+  inheritAttrs: false
+})
+
 const { name = 'placeholder', collection = 'duotone', variant } = defineProps<TIcon>()
 
 // const icon = `${collection}${variant ? `-${variant}/` : '/'}${name}`
@@ -20,10 +24,32 @@ const IconComponent = defineAsyncComponent(
 
 const attrs = useAttrs()
 
-const classListRoot = computed(() => [
-  'flex-shrink-0',
-  ((typeof attrs.class === 'string' && !attrs.class.match(/(^(w-)|( w-))(.)/g)) ||
-    typeof attrs.class === 'undefined') &&
-    'w-5 h-5'
-])
+const classListRoot = computed(() => {
+  const classes: (string | object)[] = ['flex-shrink-0']
+
+  // Добавляем переданные классы из $attrs
+  if (attrs.class) {
+    if (typeof attrs.class === 'string') {
+      const classArray = attrs.class.split(' ').filter(Boolean)
+      classes.push(...classArray)
+    } else if (Array.isArray(attrs.class)) {
+      classes.push(...attrs.class.filter(Boolean))
+    } else if (typeof attrs.class === 'object') {
+      classes.push(attrs.class)
+    }
+  }
+
+  // Проверяем, есть ли классы размера (w-* или h-*)
+  const classString = Array.isArray(classes)
+    ? classes.filter((c) => typeof c === 'string').join(' ')
+    : ''
+  const hasSizeClass = /(^|\s)(w-|h-)/.test(classString)
+
+  // Добавляем дефолтные размеры, если не указаны классы ширины/высоты
+  if (!hasSizeClass) {
+    classes.push('w-5', 'h-5')
+  }
+
+  return classes
+})
 </script>
